2e052a694b00520e7bb4e54dabc2990c55d7dcec
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-06-30 14:10:53 +0200

    Printing using format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 #The code will display the multiplication table from 1 to 10 for a number entered by the user
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1,11):
-    print(number," * ",i," = ",number*i)
+    print(f"{number} * {i} = {number*i}")
