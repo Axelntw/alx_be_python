01d95827b70cbbee2338338597bddfed13775fed
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-07-05 13:49:05 +0200

    Added future_date variable

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -11,7 +11,8 @@ def calculate_future_date(futurenumdays):
 def main():
     print(f"Current date and time: {display_current_datetime().strftime('%Y-%m-%d %H:%M:%S')}")
     future_number_of_days = int(input("Enter the number of days to add to the current date: "))
-    print(f"Future date: {calculate_future_date(future_number_of_days).strftime('%Y-%m-%d')}")
+    future_date = calculate_future_date(future_number_of_days)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
 
 if __name__ == "__main__":
     main()
